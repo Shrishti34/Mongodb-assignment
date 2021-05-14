99a1e49b245a125e0a1d17241500b961513698c4
Shrishti34 <82210141+Shrishti34@users.noreply.github.com>
2021-05-14 15:09:01 +0530

    Delete spring-security-jdbc directory
    Wrong file in this repository

--- spring-security-jdbc/src/main/java/com/shr/springsecurityjdbc/springsecurityjdbc/SecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.shr.springsecurityjdbc.springsecurityjdbc;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
